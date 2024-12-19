383220681f49c3e6396871ca29378a3d7ac619be
Aleksandr-cyber235 <neonneironeonneirono@gmail.com>
2024-12-19 13:42:43 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,15 +1,20 @@
-def get_multiplied_digits(number):
+ef get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
 
-result = get_multiplied_digits(40203)
-print(result)
+    # Если длина строки больше 1, продолжаем рекурсию
+    if len(str_number) > 1:
+        # Рекурсивно вызываем функцию, пропуская нули
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        # Если оставшаяся цифра только одна, возвращаем ее
+        return first if first != 0 else 1  # Вернем 1, чтобы корректно умножить на остальные цифры
+
+
+result1 = get_multiplied_digits(40203)
+print(result1)
 result2 = get_multiplied_digits(402030)
 print(result2)
-'''В result2 получается 0, хотя в задании в примере результата выполнения программы: 
-Вывод на консоль: 24. Можно в код добавить условие != 0,но про это не сказано в задании. 
-Как будто бы этот результат '24' ошибка в задании'''
